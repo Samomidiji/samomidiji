8debc27477b5b2d41e6f0b51d661000af774e67b
Samomidiji <omidijisamuel96@gmail.com>
2025-12-03 04:09:54 -0800

    Removing redundant files

--- newsite/wp-content/themes/greenshift/build/settings.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components'), 'version' => '1c3457ca4bf6a44aa27c');
--- newsite/wp-content/themes/roph/searchform.php
+++
@@ -1,4 +0,0 @@
-<form action="<?php echo esc_url(home_url( '/' )); ?>" method="GET">
-    <input type="search" name="s" class="form-control" placeholder="<?php echo esc_attr__('Search', 'roph'); ?>">
-    <button type="submit" class="search-button"><i class="ion-ios-search"></i></button>
-</form>
